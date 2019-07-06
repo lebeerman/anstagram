4b409e67a8dc57ae167ed3139110f2e319e7eb2a
dan beerman <omnidbeerman@gmail.com>
2019-07-05 18:29:05 -0600

    posting passes

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,4 +1,4 @@
-// const knex = require('../db/knex');
+const knex = require('../db/databaseConnection');
 const router = (module.exports = require('express').Router());
 
 // http://expressjs.com/en/guide/routing.html - source for more info on route handling
@@ -8,10 +8,10 @@ router.get('/', (req, res, next) => {
 });
 
 // MVP ROUTES
-// router.get('/words/:limit?', TODO);
+router.post('/:words.:json', addWords);
 // router.get('/anagrams/:word.:json/:limit?', TODO);
-// router.post('/:words.:json', TODO);
-// router.delete('/words.:json', TODO);
+// router.get('/words/:limit?', TODO);
+router.delete('/words.:json', deleteAllWords);
 // router.delete('/words/:word.:json', TODO);
 
 // STRETCH ROUTES
@@ -21,3 +21,41 @@ router.get('/', (req, res, next) => {
 // Endpoint that takes a set of words and returns whether or not they are all anagrams of each other
 // Endpoint to return all anagram groups of size >= *x*
 // Endpoint to delete a word *and all of its anagrams*
+
+const getLettersId = word =>
+  word
+    .split('')
+    .sort()
+    .join('');
+
+function addWords(req, res, next) {
+  // console.log('REQ Body: ', req.body);
+  const newWords = req.body.words
+    ? req.body.words.map(word => ({
+        letters_id: getLettersId(word),
+        word,
+      }))
+    : next();
+  console.log({ newWords });
+  knex('words')
+    .insert(newWords)
+    .then(item => {
+      // console.log({ item });
+      res.status(201).json({ data: newWords });
+    })
+    .catch(next);
+}
+
+function deleteAllWords(req, res, next) {
+  // console.log('TESTING DELETE WORDS');
+  // On Multiple DELETE - should there be different handling? Best Practice?
+  knex('words')
+    .select('*')
+    .delete()
+    .then(count =>
+      count >= 0
+        ? res.status(204).json({ message: `Removed ${count}, No Content` })
+        : res.status(204).json({ message: 'Nothing deleted!' })
+    )
+    .catch(next);
+}
